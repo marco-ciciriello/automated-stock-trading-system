c0b89bb0f294610e5c71bd80ed6c2820cec26a1d
Marco Ciciriello <68699414+marco-ciciriello@users.noreply.github.com>
2020-11-19 16:58:03 +0000

    feat: added trade notification emails

--- opening_range_breakout.py
+++ opening_range_breakout.py
@@ -3,9 +3,14 @@
 
 import alpaca_trade_api as tradeapi
 import config
+import smtplib
 import sqlite3
+import ssl
 
 from datetime import date
+
+# Create secure SSL context
+context = ssl.create_default_context()
 
 connection = sqlite3.connect(config.DB_FILE)
 connection.row_factory = sqlite3.Row
@@ -31,8 +36,9 @@ start_minute_bar = f'{current_date} 09:30:00-04:00'
 end_minute_bar = f'{current_date} 09:45:00-04:00'
 
 api = tradeapi.REST(config.API_KEY, config.API_SECRET, base_url=config.API_URL)
-orders = api.list_orders(status='all', limit=500, after=f'{current_date}T13:30:00Z')
+orders = api.list_orders()
 existing_order_symbols = [order.symbol for order in orders]
+messages = []
 
 for symbol in symbols:
     minute_bars = api.polygon.historic_agg_v2(symbol, 1, 'minute', _from=current_date, to=current_date).df
@@ -49,7 +55,7 @@ for symbol in symbols:
     if not after_opening_range_breakout.empty:
         if not symbol in existing_order_symbols:
             limit_price = after_opening_range_breakout.iloc[0]['close']
-            print(f'Placing order for {symbol} at {limit_price}, closed_above {opening_range_high} at {after_opening_range_breakout.iloc[0]})
+            messages.append(f'Placing order for {symbol} at {limit_price}, closed_above {opening_range_high}\n\n{after_opening_range_breakout.iloc[0]}\n\n)
             api.submit_order(
                 symbol=symbol,
                 side='buy',
@@ -67,3 +73,9 @@ for symbol in symbols:
             )
         else:
             print(f'An order for {symbol} already exists, skipping...')
+
+with smtplib.SMTP_SSL(config.EMAIL_HOST, config.EMAIL_PORT, context=context) as server:
+    email_message = f'Subject: Trade notifications for {current_date}\n\n'
+    email_message += '\n\n'.join(messages)
+    server.login(config.EMAIL_ADDRESS, config.EMAIL_PASSWORD)
+    server.sendmail(config.EMAIL_ADDRESS, config.EMAIL_ADDRESS, email_message)
